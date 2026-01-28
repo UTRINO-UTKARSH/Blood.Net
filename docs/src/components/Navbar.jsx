eabb2476d42a5645ea208fc6e816cada9d7ba2ba
Utrino <utkarshbhaskar10@gmail.com>
2026-01-28 16:33:05 +0530

    new

---
+++ docs/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex blood rounded-full mx-10 items-center justify-between py-3 px-8 shadow-xl'>
+    {/* 1. Logo Section */}
+      <div className='text-2xl cursor-pointer font-bold text-red-600 flex items-center gap-2'>
+        <span className='bg-red-600 text-white p-1 rounded-full'>🩸</span>
+        BLOOD.NET
+      </div>
+      <ul className='flex items-center gap-10 font-medium text-black'>
+        <li className='hover:text-red-600 cursor-pointer transition-colors'>Home</li>
+        <li className='hover:text-red-600 cursor-pointer transition-colors'>Donate</li>
+        <li className='hover:text-red-600 cursor-pointer transition-colors'>About Us</li>
+        <li className='hover:text-red-600 cursor-pointer transition-colors'>Contact</li>
+      </ul>
+
+      {/* 3. Login Button */}
+      <div>
+        <button className='bg-red-600 text-white px-6 py-2 rounded-full font-semibold hover:bg-red-700 transition-all shadow-md active:scale-95'>
+          Login
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
