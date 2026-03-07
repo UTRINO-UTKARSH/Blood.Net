1c70ee286f7adb07a63185ba122f80849e38de27
Utrino <utkarshbhaskar10@gmail.com>
2026-03-07 17:32:50 +0530

    Card design completed

--- version2/src/components/Cards.jsx
+++ version2/src/components/Cards.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Cards = () => {
   return (
-    <div className='container text-white bg-white/30 px-10 py-20 w-70 h-100 rounded'>
-        
+    <div className='container flex gap-10 flex-col relative text-white bg-white/30 px-3 pt-3 w-72 h-100 rounded-4xl'>
+      <div className='flex gap-4'>
+        <div className='logo border-2 w-16 h-16 overflow-hidden rounded-full' >
+          <img className='w-full h-full object-cover' src="https://img.freepik.com/free-vector/hospital-logo-design-vector-medical-cross_53876-136743.jpg?semt=ais_rp_50_assets&w=740&q=80" alt="logo" />
+        </div>
+        <div className='py-4 text-5xl '>Title</div>
+      </div>
+      <div className='text-xl'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam repellendus rem necessitatibus ut tenetur consectetur, consequuntur voluptate qui inventore iure.</p>
+      </div>
+      <div className='flex justify-center'>
+        <input type="button" value="Button" className='border px-9 py-2 rounded-lg hover:bg-white hover:text-black hover:transition-all hover:scale-120' />
+      </div>
     </div>
   )
 }
--- version2/src/components/OurFeatures.jsx
+++ version2/src/components/OurFeatures.jsx
@@ -5,7 +5,7 @@ const OurFeatures = () => {
   return (
     <div className='min-h-screen intro relative py-20 px-10 justify-center  items-center '>
       <h2 className='text-white title text-7xl font-extrabold text-center mb-10'>Our Features</h2>
-      <div className='flex justify-center flex-wrap gap-5'>
+      <div className='flex justify-center flex-wrap gap-6'>
 
         <Cards />
         <Cards />
