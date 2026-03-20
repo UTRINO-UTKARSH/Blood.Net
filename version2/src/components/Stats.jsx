599c9cd67dbe045340ac50577350783a1d7bdb76
Utrino <utkarshbhaskar10@gmail.com>
2026-03-20 10:42:54 +0530

    first design of  stats

--- version2/src/components/Stats.jsx
+++ version2/src/components/Stats.jsx
@@ -2,17 +2,32 @@ import React from 'react'
 
 const Stats = () => {
   return (
-    <div className='relative text-white z-10'>
-        <div className='heading text-white flex flex-col justify-center gap-6 items-center '>
-            <span className='text-7xl'>Why it matters?</span>
-            <span className='text-xl'>Because Every second counts</span>
+    <div className='stats intro bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-zinc-900 to-black relative flex flex-col text-white z-10'>
+      <div className='heading text-white flex flex-col justify-center gap-6 items-center '>
+        <span className='text-7xl font-bold tracking-tighter'>Why it matters?</span>
+        <span className='text-xl font-semibold'>Because Every second counts</span>
+      </div>
+      <div className='container  w-full text-center justify-center items-center my-35 mx-30 grid grid-cols-3'>
+        <div className='flex-col flex'>
+          <span className='text-7xl'>3+</span>
+          <span className='text-4xl  tracking-wide'>Lives Saved</span>
+          <span className='text-2xl'>Per Donation</span>
         </div>
-        <div className='container w-full text-center justify-center items-center border my-45 mx-30 grid grid-cols-3'>
-            <div className=''>1
-            </div>
-            <div className=''>2</div>
-            <div className=''>3</div>
+        <div className='flex-col flex'>
+          <span className='text-7xl'>2s</span>
+          <span className='text-4xl tracking-wide'>Someone</span>
+          <span className='text-2xl'>Needs Blood Access</span>
         </div>
+        <div className='flex-col flex'>
+          <span className='text-7xl'>24/7</span>
+          <span className='text-4xl tracking-wide'>Emergency</span>
+          <span className='text-2xl '>Access Available</span>
+        </div>
+      </div>
+      <div className='flex justify-around'>
+      <button className='text-4xl border px-6 py-3 transition-all hover:bg-amber-50 hover:text-black hover:scale-110 hover:shadow-[0_0_25px_rgba(220,38,38,0.5)] hover:rounded-full rounded-xl'>Join the network</button>
+      </div>
+        
     </div>
   )
 }
