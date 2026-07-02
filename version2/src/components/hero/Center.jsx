bf30b5a82bc6f142ec0684014b45fd5ebf490b99
Utrino <utkarshbhaskar10@gmail.com>
2026-07-02 11:29:50 +0530

    new hero design:

--- version2/src/components/hero/Center.jsx
+++ version2/src/components/hero/Center.jsx
@@ -12,7 +12,7 @@ const Center = () => {
                 </h1>
 
                 <h2 className='mt-4 text-white head2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium'>
-                    Blood, Hospitals and
+                   <span className='font-bold text-transparent bg-clip-text bg-linear-to-b from-[#FF6B6B] via-[#EF4444] to-[#B91C1C]'> Blood, Hospitals </span>and
                 </h2>
 
                 <h2 className='mt-2 semi1 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl'>
@@ -28,10 +28,9 @@ const Center = () => {
             {/* Subtitle */}
             <div className='showup flex flex-col items-center text-center mt-10 px-4'>
 
-                <span className='text-sm sm:text-base md:text-lg lg:text-xl font-bold bg-gray-200 rounded-full px-6 py-2 text-black'>
+             <span className=" inline-flex items-center gap-2 text-sm sm:text-base md:text-lg font-bold bg-white/89 text-zinc-900 rounded-full px-7 py-3 border border-white/20 shadow-[0_8px_30px_rgba(255,255,255,0.08)]backdrop-blur-md">
                     Connecting Care. Saving Lives.
                 </span>
-
                 <p className='mt-4 text-white text-sm sm:text-base md:text-lg lg:text-xl max-w-3xl italic font-medium'>
                     Find blood donors, blood banks, hospitals and emergency
                     services when every second matters.
